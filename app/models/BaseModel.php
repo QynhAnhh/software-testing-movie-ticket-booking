0302daa076f1b35fac3cfe68e0ec7d94f5730c66
nhanhehehe <bengame2006@gmail.com>
2026-06-27 21:21:05 +0700

    adding create and upate CRUD

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -29,4 +29,27 @@ class BaseModel
         $stmt = $this->db->prepare("DELETE FROM {$this->table} WHERE id = :id");
         return $stmt->execute(['id' => $id]);
     }
+
+    public function create($data)
+    {
+        $columns = implode(', ', array_keys($data));
+        $placeholders = ':' . implode(', :', array_keys($data));
+        $sql = "INSERT INTO {$this->table} ({$columns}) VALUES ({$placeholders})";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($data);
+        return $this->db->lastInsertId();
+    }
+
+    public function update($id, $data)
+    {
+        $set = [];
+        foreach ($data as $key => $value) {
+            $set[] = "{$key} = :{$key}";
+        }
+        $setStr = implode(', ', $set);
+        $data['id'] = $id;
+        $sql = "UPDATE {$this->table} SET {$setStr} WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute($data);
+    }
 }
