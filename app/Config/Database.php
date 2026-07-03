671491d5fd558da72c848fce7e60afd7bc47da79
Phan Văn Tiến <tienpv7888@ut.edu.vn>
2026-07-03 20:00:13 +0700

    change port 3306 to 3307

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -12,7 +12,7 @@ class Database {
             $database = 'movie_ticket_booking';
             $port = 3307;
 
-            $conn = mysqli_connect($host, $username, $password, $database);
+            $conn = mysqli_connect($host, $username, $password, $database, $port);
             if (!$conn) {
                 die("Connection failed: " . mysqli_connect_error());
             }
