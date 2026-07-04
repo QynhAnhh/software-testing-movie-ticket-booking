c1f1cd8dfecebb3345b39752db21c0837e40d556
nhanhehehe <bengame2006@gmail.com>
2026-07-05 01:09:49 +0700

    font css to roboto

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -30,7 +30,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;800&display=swap" rel="stylesheet">
 
     <!-- Bootstrap 5 CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
