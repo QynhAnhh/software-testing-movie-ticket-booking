3ba053215b2e30c68afff2091aca41baeba5f364
Do Nguyen Quynh Anh <irisdo.qa@gmail.com>
2026-08-02 19:25:02 +0700

    Fixes KAN-34: Siết chặt chính sách CORS bằng whitelist domains

--- api/init.php
+++ api/init.php
@@ -1,6 +1,17 @@
 <?php
-// Bật CORS (cho phép Postman hoặc frontend khác tên miền gọi API)
-header("Access-Control-Allow-Origin: *");
+// Siết chặt CORS (chỉ cho phép các domain cụ thể)
+$allowed_origins = [
+    'http://localhost',
+    'http://localhost:8080',
+    'http://127.0.0.1'
+];
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    if (in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+        header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    }
+}
+
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
