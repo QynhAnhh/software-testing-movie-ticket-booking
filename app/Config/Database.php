06ce7fd765d02eab391923795be2d0e8703ddc6d
Do Nguyen Quynh Anh <irisdo.qa@gmail.com>
2026-08-25 23:17:20 +0700

    Restore require_once in Database config

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,7 +24,7 @@ class Database
                 );
             }
 
-            $config = require $configFile;
+            $config = require_once $configFile;
 
             $requiredKeys = [
                 'host',
